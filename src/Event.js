0b0ba0d6eb645c104bd89da6b1db633024e8835a
codilot <30231189+Codilot@users.noreply.github.com>
2021-01-12 15:20:20 +0100

    Add Event List components

---
+++ src/Event.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Event = (props) => {
+    return (
+        <li>{props.event}</li>
+    )
+}
+
+export default Event
--- src/EventList.js
+++ src/EventList.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Event from './Event'
 
-const Eventlist = () => (
+const Eventlist = (props) => (
     <div>
         <h1>My Events</h1>
         <ul className="EventList">
+            {props.events.map((event) => 
+               <Event event={event} key={event.id} />
+            )}
         </ul>
     </div>
 )
